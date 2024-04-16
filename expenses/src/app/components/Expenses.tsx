2da15a1b1137a6d90ca396c05fd24196f8ce952e
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-04-16 22:56:48 +0000

    Adding value works

--- expenses/src/app/components/Expenses.tsx
+++ expenses/src/app/components/Expenses.tsx
@@ -5,6 +5,8 @@ import { api } from "../api/api";
 
 const Expenses = () => {
   const [expenses, setExpenses] = useState<Expense[]>();
+  const [newName, setNewName] = useState("");
+  const [newCost, setNewCost] = useState("");
 
   const fetchExpenses = async () => {
     const fetchedExpenses = await api.getExpenses();
@@ -14,22 +16,53 @@ const Expenses = () => {
     fetchExpenses();
   }, []);
 
+  const addExpense = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const expenseObject = {
+      name: newName,
+      cost: newCost,
+    };
+    api.postExpenses(expenseObject);
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewName(event.target.value);
+  };
+
+  const handleCostChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewCost(event.target.value);
+  };
+
   return (
     <>
       <div>
         <div className="input">
-          <h1>Add Expense</h1>
-          <div className="input-add">
-            <div className="name">
-              <p>Name</p>
-              <input type="text" name="name" />
+          <form onSubmit={addExpense}>
+            <h1>Add Expense</h1>
+            <div className="input-add">
+              <div className="name">
+                <p>Name</p>
+                <input
+                  type="text"
+                  name="name"
+                  value={newName}
+                  onChange={handleNameChange}
+                  required
+                />
+              </div>
+              <div className="cost">
+                <p>Cost</p>
+                <input
+                  type="number"
+                  name="cost"
+                  value={newCost}
+                  onChange={handleCostChange}
+                  required
+                />
+              </div>
+              <button type="submit">Add</button>
             </div>
-            <div className="cost">
-              <p>Cost</p>
-              <input type="text" name="cost" />
-            </div>
-            <button>Add</button>
-          </div>
+          </form>
           <h1>Stats</h1>
           <div className="stats">
             <p>Sum</p>
