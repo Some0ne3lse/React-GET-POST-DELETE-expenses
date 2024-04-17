4af32b38318e49d6afc14160432db9e2cd20f994
Some0ne3lse <143652507+Some0ne3lse@users.noreply.github.com>
2024-04-17 13:31:44 +0000

    Basic css done

--- expenses/src/app/components/Expenses.tsx
+++ expenses/src/app/components/Expenses.tsx
@@ -42,66 +42,71 @@ const Expenses = () => {
   };
 
   return (
-    <>
-      <div>
+    <div className="entire-screen">
+      <div className="contents">
         <div className="input">
-          <form onSubmit={addExpense}>
-            <h1>Add Expense</h1>
-            <div className="input-add">
-              <div className="name">
-                <p>Name</p>
-                <input
-                  type="text"
-                  name="name"
-                  value={newName}
-                  onChange={handleNameChange}
-                  required
-                />
-              </div>
-              <div className="cost">
-                <p>Cost</p>
-                <input
-                  type="number"
-                  name="cost"
-                  value={newCost}
-                  onChange={handleCostChange}
-                  required
-                />
-              </div>
-              <button type="submit">Add</button>
-            </div>
+          <h1 className="header">Add Expense</h1>
+          <form onSubmit={addExpense} className="form">
+            <p className="name-text">Name</p>
+            <input
+              type="text"
+              name="name"
+              value={newName}
+              onChange={handleNameChange}
+              className="name-input"
+              required
+            />
+            <p className="cost-text">Cost</p>
+            <input
+              type="number"
+              name="cost"
+              value={newCost}
+              onChange={handleCostChange}
+              className="cost-input"
+              required
+            />
+            <button type="submit" className="add-button">
+              Add
+            </button>
           </form>
-          <h1>Stats</h1>
+          <h1 className="header">Stats</h1>
           <div className="stats">
             <p>
-              Sum{" "}
+              Sum:{" "}
               {expenses?.reduce(
                 (accumulator, currentValue) => accumulator + currentValue.cost,
                 0
               )}
             </p>
-            <p>Count {expenses?.length}</p>
+            <p>Count: {expenses?.length}</p>
           </div>
         </div>
-        <div className="content">
+        <div className="contents-card">
           <ul className="expenses-list">
             {expenses?.map((expense) => (
               <li key={expense.id} className="card">
                 <div className="card-text">
-                  <div className="card-name">Name: {expense.name}</div>
-                  <div className="card-cost">Cost: {expense.cost}</div>
+                  <div className="card-name">
+                    <div className="card-name-text">Name: </div>
+                    <div>{expense.name}</div>
+                  </div>
+                  <div className="card-cost">
+                    <div className="card-cost-text">Cost: </div>
+                    <div>{expense.cost}</div>
+                  </div>
                 </div>
                 <FontAwesomeIcon
                   icon={faX}
                   onClick={() => handleDelete(expense.id)}
                   style={{ cursor: "pointer" }}
+                  className="icon"
                 />
               </li>
             ))}
           </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
